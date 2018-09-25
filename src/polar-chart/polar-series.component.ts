7214ccceed471605d48368f814083bac50233fae
johann.rekowski <johann.rekowski@mego.gmbh>
2018-09-25 13:02:16 +0200

    feat(tooltip): allow access to custom properties in tooltip templates

--- src/polar-chart/polar-series.component.ts
+++ src/polar-chart/polar-series.component.ts
@@ -113,11 +113,8 @@ export class PolarSeriesComponent implements OnChanges {
 
       const color = this.colors.getColor(linearScaleType ? Math.abs(value) : seriesName);
 
-      const cData = {
-        series: seriesName,
-        value,
-        name: d.name
-      };
+      const cData = d;
+      cData.series = seriesName;
 
       return {
         data: cData,
